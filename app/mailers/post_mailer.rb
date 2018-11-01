6975b14e65fd42a4f606d885d44de8207a825245
dubilla <dan.ubilla@gmail.com>
2018-10-31 22:19:55 -0400

    Add league name to the post titles

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,7 +2,7 @@ class PostMailer < ApplicationMailer
   def new
     @post = params[:post].decorate
     @owners = Owner.all
-    mail(to: @owners.map(&:email), subject: @post.title)
+    mail(to: @owners.map(&:email), subject: "NBANHBL: #{@post.title}")
     @post.update_attributes(sent_at: DateTime.now)
   end
 end
